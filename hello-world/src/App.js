f08b85c263df9c49a9c31c0421d051459bf2abba
kmskpraveen <bgkmskpraveenmb@gmail.com>
2020-04-15 12:57:14 +0530

    13 - Event Handling

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -6,10 +6,14 @@ import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
+      <FunctionClick/>
+      <ClassClick/>
       {/* <Counter/> */}
       {/* <Message/> */}
       {/* <Greet name="Bruce" heroName="Batman">
@@ -18,8 +22,8 @@ function App() {
       <Greet name="Clark" heroName="Superman">
         <button>Action</button>
       </Greet> */}
-      <Greet name="Diana" heroName="Woner woman"/>
-      <Welcome name="Bruce" heroName="Batman"/>
+      {/* <Greet name="Diana" heroName="Woner woman"/> */}
+      {/* <Welcome name="Bruce" heroName="Batman"/> */}
       {/* <Welcome name="Clark" heroName="Superman"/>
       <Welcome name="Diana" heroName="Woner woman"/> */}
       {/* <Hello/> */}
