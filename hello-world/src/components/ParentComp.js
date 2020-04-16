4e112861dccd0c45dc91ca8a4099705308baf84b
kmskpraveen <bgkmskpraveenmb@gmail.com>
2020-04-17 00:15:16 +0530

    26 - Pure Components

---
+++ hello-world/src/components/ParentComp.js
@@ -0,0 +1,35 @@
+import React, { Component, PureComponent } from 'react'
+import RegComp from './RegComp'
+import PureComp from './PureComp'
+
+class ParentComp extends PureComponent {
+// class ParentComp extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             name: 'advance'
+        }
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                name: 'advance'
+            })
+        }, 2000)
+    }
+    
+    render() {
+        console.log('====Parent component render====')
+        return (
+            <div>
+                Parent Component
+                <RegComp name={this.state.name}/>
+                <PureComp name={this.state.name}/>
+            </div>
+        )
+    }
+}
+
+export default ParentComp
