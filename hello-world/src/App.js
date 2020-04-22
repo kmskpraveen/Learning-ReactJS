3cfffcc6e1c0e53751e6dc44a38d4ff769da3175
kmskpraveen <bgkmskpraveenmb@gmail.com>
2020-04-22 12:29:51 +0530

    33 - Higher Order Components (Part 1)

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -28,11 +28,15 @@ import FRParentInput from './components/FRParentInput';
 import PortalDemo from './components/PortalDemo';
 import Hero from './components/Hero';
 import ErrorBoundary from './components/ErrorBoundary';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 
 function App() {
   return (
     <div className="App">
-      <ErrorBoundary>
+      <ClickCounter />
+      <HoverCounter />
+      {/* <ErrorBoundary>
         <Hero heroName="Batman" />
       </ErrorBoundary>
 
@@ -42,7 +46,7 @@ function App() {
       
       <ErrorBoundary>
         <Hero heroName="Joker" />
-      </ErrorBoundary>
+      </ErrorBoundary> */}
       {/* <PortalDemo /> */}
       {/* <FRParentInput /> */}
       {/* <FocusInput /> */}
