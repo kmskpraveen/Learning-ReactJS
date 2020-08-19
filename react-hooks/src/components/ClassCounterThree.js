f26507f7fb633ebeb0eedf3ab6b6495444aebb90
kmskpraveen <bgkmskpraveenmb@gmail.com>
2020-08-19 14:48:58 +0530

    8 - Conditionally run effects

--- react-hooks/src/components/ClassCounterThree.js
+++ react-hooks/src/components/ClassCounterThree.js
@@ -5,7 +5,8 @@ class ClassCounterThree extends Component {
         super(props)
     
         this.state = {
-             count: 0
+             count: 0,
+             name: ''
         }
     }
 
@@ -13,13 +14,19 @@ class ClassCounterThree extends Component {
         document.title = `Clicked ${this.state.count} times`;
     }
 
-    componentDidUpdate() {
-        document.title = `Clicked ${this.state.count} times`;
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.count !== this.state.count) {
+            console.log('Updating document title');
+            document.title = `Clicked ${this.state.count} times`;    
+        }
     }
 
     render() {
         return (
             <div>
+                <input type='text' onChange={e => this.setState({
+                    name: e.target.value
+                })} value={this.state.name}/>
                 <button onClick={() => this.setState({
                     count: this.state.count + 1
                 })}>Click {this.state.count} times</button>
